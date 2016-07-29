7c93c1e439dfa06d0c836e3b413bbab2abf02790
Markus Kobler <markus@distinctive.co>
2016-07-29 12:06:35 +0100

    Work in process heka build

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+set -eux
+
+## TODO: move out of here
+dnf -y install golang
+
+BASE=$PWD
+SRC=$PWD/src
+OUT=$PWD/heka-build
+
+
+cd $SRC
+source build.sh
+
+mkdir $OUT
+cp build/heka/bin/hekad $OUT/
+
